639d3a26349eac8dc3b8ff293132bed4f79ce3fe
kwu16 <kevin.wu4@hilton.com>
2022-07-25 11:49:33 -0400

    Updated description for the overall package.

--- R/matches.R
+++ R/matches.R
@@ -1,16 +1,28 @@
 
 # 1. result based on transmuting a data frame
-uss_make_matches <- function(data_engsoc) {
+#' Make a standard league-play tibble
+#'
+#' @param data_engsoc data frame from engsoccerdata package
+#' @param country Spain, Italy, Germany, England & Holland
+#'
+#' @return a tibble with columns: `country`, `date`, `season`, `tier`, `home`, `visitor`, `goals_home`, `goals_visitor`.
+#' @export
+#'
+#' @examples
+#' uss_make_matches(engsoccerdata::spain, "Spain")
+uss_make_matches <- function(data_engsoc, country) {
   result <-
     data_engsoc |>
     tibble::as_tibble() |>
-    dplyr::transmute(tier = factor(tier, levels = c("1", "2", "3", "4")),
-                     season = as.integer(Season),
-                     date = as.Date(Date),
-                     home = as.character(home),
-                     visitor = as.character(visitor),
-                     goals_home = as.integer(hgoal),
-                     goals_visitor = as.integer(vgoal)
-                    )
+    dplyr::transmute(
+      country = as.character(country),
+      tier = factor(tier, levels = c("1", "2", "3", "4")),
+      season = as.integer(Season),
+      date = as.Date(Date),
+      home = as.character(home),
+      visitor = as.character(visitor),
+      goals_home = as.integer(hgoal),
+      goals_visitor = as.integer(vgoal)
+    )
   result
 }
