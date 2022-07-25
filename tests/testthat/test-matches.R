9bee363ee73da25c888b9477f4d842c585c33b30
kwu16 <kevin.wu4@hilton.com>
2022-07-25 14:42:02 -0400

    Added tests

---
+++ tests/testthat/test-matches.R
@@ -0,0 +1,13 @@
+test_that("uss_make_matches works", {
+
+  # use the function
+  italy <- uss_make_matches(engsoccerdata::italy, "Italy")
+
+  expect_true(tibble::is_tibble(italy))
+  expect_named(object = italy,
+               expected = c('country', 'tier', 'season', 'date', 'home', 'visitor', 'goals_home', 'goals_visitor')
+               )
+  expect_identical(object = unique(italy$country),
+                   expected = c('Italy'))
+
+})
